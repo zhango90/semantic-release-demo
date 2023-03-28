a60c1a0b670f948ad45faa2ab9f54e6dcaca7d66
Yi Zhang <yi.zhang@bitrock.it>
2023-03-28 21:52:34 +0200

    feat: change text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacto!!!!!!!!!!!!
+          Learn Reacto in italiano!!!!!!!!!!!!
         </a>
       </header>
       hello!
