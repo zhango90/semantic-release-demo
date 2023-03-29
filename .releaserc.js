8c37d318ff8654baa825ad4b357452b06c857a20
Yi Zhang <yi.zhang@bitrock.it>
2023-03-29 19:13:34 +0200

    ci: change

---
+++ .releaserc.js
@@ -0,0 +1,15 @@
+exports.default = {
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "master",
+    "next",
+    {
+      name: "beta",
+      prerelease: true
+    },
+    {
+      name: "feature/*",
+      prerelease: '${name.replace(/^feature\\//g, "")}'
+    }
+  ]
+};
